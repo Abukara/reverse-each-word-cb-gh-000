666ca2ed0e38cb6e6a906ab0d0be17c52eebf4c3
Lukas F <lukas.forst48@googlemail.com>
2018-09-16 16:56:15 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+def reverse_each_word(phrase)
+  array = phrase.split(' ')
+  result = ""
+  array.each_with_index{ |string, index|
+  last_index = array.size - 1
+  if index == last_index
+    result << "#{string.reverse()}"
+  else
+    result << "#{string.reverse()} "
+  end
+  }
+  array.collect
+  return result
+end
