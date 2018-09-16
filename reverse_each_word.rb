89a5b46e0cfa0055147190501f204bb251a998cd
Lukas F <lukas.forst48@googlemail.com>
2018-09-16 17:00:45 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,3 @@
-def reverse_each_word(phrase)
-  array = phrase.split(' ')
-  result = ""
-  array.each_with_index{ |string, index|
-  last_index = array.size - 1
-  if index == last_index
-    result << "#{string.reverse()}"
-  else
-    result << "#{string.reverse()} "
-  end
-  }
-  array.collect
-  return result
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
